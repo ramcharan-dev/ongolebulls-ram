309da924845572e9581ac96b9230e04255c6c304
Ambika145 <139555252+Ambika145@users.noreply.github.com>
2025-08-23 16:22:34 +0530

    have aaded some chnages in the registerrequest

--- src/main/java/dev/ongolebulls/model/User.java
+++ src/main/java/dev/ongolebulls/model/User.java
@@ -1,3 +1,4 @@
+/*
 package dev.ongolebulls.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -71,5 +72,164 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+}*/
+package dev.ongolebulls.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @JsonProperty("fname")
+    private String firstName;
+
+    @JsonProperty("lname")
+    private String lastName;
+
+    private String mobile;
+    private String password;
+
+    // ==== Extra Fields for multi-step registration ====
+    private String panNumber;
+    private String aadhaarNumber;
+    private String email;
+
+    private String accountHolderName;
+    private String bankName;
+    private String ifscCode;
+    private String accountNumberEncrypted;
+
+    private String occupation;
+    private String employerName;
+    private String incomeRange;
+
+    private boolean declarationAccepted;
+
+    // ===== Getters and Setters =====
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPanNumber() {
+        return panNumber;
+    }
+    public void setPanNumber(String panNumber) {
+        this.panNumber = panNumber;
+    }
+
+    public String getAadhaarNumber() {
+        return aadhaarNumber;
+    }
+    public void setAadhaarNumber(String aadhaarNumber) {
+        this.aadhaarNumber = aadhaarNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    public String getAccountNumberEncrypted() {
+        return accountNumberEncrypted;
+    }
+    public void setAccountNumberEncrypted(String accountNumberEncrypted) {
+        this.accountNumberEncrypted = accountNumberEncrypted;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getEmployerName() {
+        return employerName;
+    }
+    public void setEmployerName(String employerName) {
+        this.employerName = employerName;
+    }
+
+    public String getIncomeRange() {
+        return incomeRange;
+    }
+    public void setIncomeRange(String incomeRange) {
+        this.incomeRange = incomeRange;
+    }
+
+    public boolean isDeclarationAccepted() {
+        return declarationAccepted;
+    }
+    public void setDeclarationAccepted(boolean declarationAccepted) {
+        this.declarationAccepted = declarationAccepted;
     }
 }
