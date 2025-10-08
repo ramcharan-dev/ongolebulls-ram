f907ef99b14c1ee493f5464f46b097bc067a6127
Ambika145 <139555252+Ambika145@users.noreply.github.com>
2025-10-08 15:09:33 +0530

    port number changed in the contact controller

--- src/main/java/dev/ongolebulls/controller/ContactController.java
+++ src/main/java/dev/ongolebulls/controller/ContactController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.*;
 //        }
 //    }
 //}
-@CrossOrigin(origins = {"http://localhost:8085", "https://www.ongolebullsinvest.com"}) // Allow local and production frontend
+@CrossOrigin(origins = {"http://localhost:8080", "https://www.ongolebullsinvest.com"}) // Allow local and production frontend
 @RestController
 @RequestMapping("/api/contact")
 @RequiredArgsConstructor
