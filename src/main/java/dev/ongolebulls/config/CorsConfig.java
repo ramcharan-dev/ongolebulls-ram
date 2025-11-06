a87757081fddb27e6cc836e81b976ad20c773020
TharunKannekanti <tharunkannekanti27@gmail.com>
2025-11-06 17:42:12 +0530

    Revert "Merge branch 'Dev' into DevlopmentByTarun"
    This reverts commit 983e57f7db6dc8cf4dc1ac21ff466be117527662, reversing
    changes made to ac925e465ca33b14e0ab559632e2545812205cc4.

--- src/main/java/dev/ongolebulls/config/CorsConfig.java
+++ src/main/java/dev/ongolebulls/config/CorsConfig.java
@@ -24,7 +24,6 @@ package dev.ongolebulls.config;//package dev.ongolebulls.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -40,11 +39,5 @@ public class CorsConfig {
                         .allowedHeaders("*");
             }
         };
-    }
-
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Serve uploaded files
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
     }
 }
