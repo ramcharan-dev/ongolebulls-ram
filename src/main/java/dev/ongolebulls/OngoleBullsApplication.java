2e986f9a1a5bf12c606f62e561a83e4d2976b111
Chandrika-12p <dasaripoornachandrika@innomaxsol.com>
2026-01-05 18:56:57 +0530

    fixed the subscribe us for both blog and uubscribe us also added the favicon and changed SEBI to AMFI registered

--- src/main/java/dev/ongolebulls/OngoleBullsApplication.java
+++ src/main/java/dev/ongolebulls/OngoleBullsApplication.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "dev.ongolebulls.repository")
 public class OngoleBullsApplication {
 	public static void main(String[] args) {
-		System.out.println("application started=======");
 		SpringApplication.run(OngoleBullsApplication.class, args);
 	}
 }
